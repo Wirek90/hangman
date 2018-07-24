66849d87bb762f11a6de40888f8b2093e476483f
tomaszgubernat <tomekgubernat@gmail.com>
2018-07-24 11:57:52 +0200

    new start

---
+++ src/com/codecool/src/Main.java
@@ -0,0 +1,9 @@
+package com.codecool.src;
+
+class Main {
+
+    public static void main(String[] args){
+
+
+    }
+}
---
+++ src/com/codecool/src/Model/RandomWordsGenerator.java
@@ -0,0 +1,29 @@
+package Model;
+
+public class RandomWordsGenerator {
+<<<<<<< HEAD
+
+String[] wordsWith6Letters = {"avenue", "killer", "harass", "member", "unlike", "mosque", "choose", "retain", "shiver",
+        "bomber", "bitter", "strong", "ribbon", "mother", "deadly", "resign", "deport", "reject", "create", "banana"};
+
+String[] wordsWith10Letters = {"repetition", "occupation", "gregarious", "revolution", "engagement", "multimedia",
+        "atmosphere", "correspond", "confidence", "experiment", "dependence", "appearance", "obligation", "hypothesis",
+        "conference", "deficiency", "generation", "appreciate", "retirement", "compliance"};
+
+String[] wordsWith12Letters = {"constituency", "reproduction", "interference", "headquarters", "conversation",
+        "jurisdiction", "refrigerator", "discriminate", "circumstance", "architecture", "conventional", "intervention",
+        "accumulation", "satisfaction", "manufacturer", "intelligence", "contribution", "disagreement", "presentation",
+        "registration"};
+
+
+        public String getWord() {
+                String randomWord = wordsWith6Letters[(int) (Math.random() * wordsWith6Letters.length)];
+                System.out.println(randomWord);
+                return randomWord;
+        }
+}
+=======
+    // stworzyc liste slow
+    // stworzyc
+}
+>>>>>>> 0b95df8932e69e2f4760abb830cb8d84044cfc7b
